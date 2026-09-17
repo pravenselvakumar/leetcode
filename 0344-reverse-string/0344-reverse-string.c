c9d017d5ad2bca643ca8f2673391caee7a2bc6cb
Praven <pravenkumar.s123@gmail.com>
2026-09-17 11:10:15 +0530

    Time: 0 ms (100%), Space: 17.7 MB (75.2%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.c
@@ -0,0 +1,13 @@
+void reverseString(char* s, int size) {
+    int left=0;
+    int right =size-1;
+    while(left<right)
+    {
+        char temp=s[left];
+        s[left]=s[right];
+        s[right]=temp;
+        left++;
+        right--;
+    }
+    
+}
