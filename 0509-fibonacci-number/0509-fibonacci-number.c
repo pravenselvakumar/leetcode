79eeee5e7b32f974a6fe472900cf342e5aa85356
Praven <pravenkumar.s123@gmail.com>
2026-09-18 15:21:13 +0530

    Time: 3 ms (66.22%), Space: 8.5 MB (22.02%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -0,0 +1,15 @@
+
+
+int fib(int n){
+    int a=0,b=1,c=0;
+    for(int i=0;i<n;i++)
+    {
+        //printf("%d",a);
+         c=a+b;
+            a=b;
+            b=c;
+
+    }
+    return a;
+
+}
